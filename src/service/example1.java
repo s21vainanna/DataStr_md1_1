738ffc6e1e3d00513246b57a29b4e08d77695d09
Anna Paula Vainovska <s21vainanna@venta.lv>
2025-04-13 23:18:22 +0300

    Create example1.java

---
+++ src/service/example1.java
@@ -0,0 +1,10 @@
+(xx(xx())xx)
+[](){}
+([] { xxx } xxx () xxx )
+([{[(([{ x }])x)]}x])))
+xxxxxxxxxxxxxxxxxx
+(xx(xx())xx)]]]]]]])
+[](){}
+([] { xxx } xxx () xxx 
+([([({[(/*([{*/ x }]))]}x)])x])
+xxxxxxxxxxxxxxxxxx
